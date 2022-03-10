a96651bb432bd6e5720117e4dac92daa9e7fd836
Kamlesh Bobde <kamlesh.bobde@newvisionsoftware.in>
2022-03-10 19:49:00 +0530

    added dependencies inside inner package.json

--- src/plugins/entando-plugin/server/middlewares/ent-interceptor.js
+++ src/plugins/entando-plugin/server/middlewares/ent-interceptor.js
@@ -1,4 +1,5 @@
 jwt_decode = require("jwt-decode");
+
 axios = require("axios");
 
 const Keycloak = require("keycloak-verify").default;
@@ -19,13 +20,12 @@ module.exports = (config, { strapi }) => {
     // console.log("ctx.response:::", ctx.response);
     await next();
 
-    // const entandoKcToken = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJoOHVDQjJFbTUxeDdJS0lqSlItM252SW5UeUk3b215SnU4QXMta0RESi1NIn0.eyJleHAiOjE2NDY4OTQyOTQsImlhdCI6MTY0Njg5Mzk5NCwiYXV0aF90aW1lIjoxNjQ2ODkxNDA1LCJqdGkiOiIyYTNhZGYwZC01YjVlLTRlNzMtYjAyOC0yMTM2MDQzYTZhNzAiLCJpc3MiOiJodHRwOi8vMTkyLjE2OC40My4zLm5pcC5pby9hdXRoL3JlYWxtcy9lbnRhbmRvIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjMxYTdjYmVhLWVhNGMtNDEwMS05MzdlLTc2NzRjMTZhMTcxNSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVudGFuZG8td2ViIiwibm9uY2UiOiIzYjZiZmI5MC01MGNiLTQ1Y2YtODNmOC05YzQyYmU0ODNkYTciLCJzZXNzaW9uX3N0YXRlIjoiM2U0YzZjMzAtMGRlMy00ZjcyLWI3YWItYjM0YWJlNGI3ZmQwIiwiYWNyIjoiMCIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vMTkyLjE2OC40My4zLm5pcC5pbyJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6ImFkbWluIn0.YcrsKezF1WHrmFz9jRZ9Ho6149oI12iM9UsDnkBRAFUCh_2oB6vsIFrD260pGWA2Pohc2aKRzyZduCuG8X6izr6Au3nRUJ_Tc-3AnATav4MpJgm56MnnzO6jcMEV9jxVsoIod4jbdujczf-o5_Ng0nifO6d0m6QE9vRCfgTwgKI2caPyFp14M24K4Sthg3XOaGwsErE9SwmFxAJNzfukA6bbR3FUgM0VkWbTrKHnHoMCqpdLKsYA6Y7xvkl58juJ31_4n4JJDsG1ic8s-e3y9eicbttRWp7gIcdHAC8dbHxrMx8RQ_ctSJMX4Tf3pFd4vH6r6E0Z_U79BvjiGYuVIg";
+    // const entandoKcToken = "yJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJoOHVDQjJFbTUxeDdJS0lqSlItM252SW5UeUk3b215SnU4QXMta0RESi1NIn0.eyJleHAiOjE2NDY4OTQyOTQsImlhdCI6MTY0Njg5Mzk5NCwiYXV0aF90aW1lIjoxNjQ2ODkxNDA1LCJqdGkiOiIyYTNhZGYwZC01YjVlLTRlNzMtYjAyOC0yMTM2MDQzYTZhNzAiLCJpc3MiOiJodHRwOi8vMTkyLjE2OC40My4zLm5pcC5pby9hdXRoL3JlYWxtcy9lbnRhbmRvIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjMxYTdjYmVhLWVhNGMtNDEwMS05MzdlLTc2NzRjMTZhMTcxNSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVudGFuZG8td2ViIiwibm9uY2UiOiIzYjZiZmI5MC01MGNiLTQ1Y2YtODNmOC05YzQyYmU0ODNkYTciLCJzZXNzaW9uX3N0YXRlIjoiM2U0YzZjMzAtMGRlMy00ZjcyLWI3YWItYjM0YWJlNGI3ZmQwIiwiYWNyIjoiMCIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vMTkyLjE2OC40My4zLm5pcC5pbyJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6ImFkbWluIn0.YcrsKezF1WHrmFz9jRZ9Ho6149oI12iM9UsDnkBRAFUCh_2oB6vsIFrD260pGWA2Pohc2aKRzyZduCuG8X6izr6Au3nRUJ_Tc-3AnATav4MpJgm56MnnzO6jcMEV9jxVsoIod4jbdujczf-o5_Ng0nifO6d0m6QE9vRCfgTwgKI2caPyFp14M24K4Sthg3XOaGwsErE9SwmFxAJNzfukA6bbR3FUgM0VkWbTrKHnHoMCqpdLKsYA6Y7xvkl58juJ31_4n4JJDsG1ic8s-e3y9eicbttRWp7gIcdHAC8dbHxrMx8RQ_ctSJMX4Tf3pFd4vH6r6E0Z_U79BvjiGYuVIg";
     const entandoKcToken = ctx.request.header.entkctoken;
-
     if (entandoKcToken && !ctx.request.url.includes('/admin/login')) {
       //Decode the kc token
       let decoded = jwt_decode(entandoKcToken);
-      // console.log('Decoded kc token: ', decoded);
+      console.log('Decoded kc token: ', decoded);
 
       if (decoded) {
         let kcSuccessResponse = null;
@@ -41,7 +41,6 @@ module.exports = (config, { strapi }) => {
           });
 
         if (kcSuccessResponse) {
-          
           //Compare decoded and verified token details
           if (decoded.preferred_username === kcSuccessResponse.userName
             && decoded.email_verified === kcSuccessResponse.emailVerified
